0117ba438af0b80298c29886d217c7aec8fae3a7
Ezreal <memory_of_you@hotmail.com>
2023-03-19 01:07:32 +0800

    文档润色

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,7 @@
 ﻿import { defineUserConfig } from "vuepress";
+import { defaultTheme } from "@vuepress/theme-default";
+import { navbarZh, sidebarZh } from "./configs/index.js";
+const isProd = process.env.NODE_ENV === "production";
 
 export default defineUserConfig({
   port: 3000, // 将端口号从原来的8080修改为3000
@@ -6,4 +9,66 @@ export default defineUserConfig({
   title: "WebApiClient",
   description: "声明，调用！",
   base: "/WebApiClientWiki/", // 或者 '/'
+
+  // configure default theme
+  theme: defaultTheme({
+    logo: "/logo.png",
+    repo: "dotnetcore/WebApiClient",
+    docsDir: "docs",
+
+    // theme-level locales config
+    locales: {
+      // /**
+      //  * English locale config
+      //  */
+      // "/en/": {
+      //   // navbar
+      //   navbar: navbarEn,
+      //   // sidebar
+      //   sidebar: sidebarEn,
+      //   // page meta
+      //   editLinkText: "Edit this page on GitHub",
+      // },
+
+      /**
+       * Chinese locale config
+       */
+      "/": {
+        // navbar
+        navbar: navbarZh,
+        selectLanguageName: "简体中文",
+        selectLanguageText: "选择语言",
+        selectLanguageAriaLabel: "选择语言",
+        // sidebar
+        sidebar: sidebarZh,
+        // page meta
+        editLinkText: "在 GitHub 上编辑此页",
+        lastUpdatedText: "上次更新",
+        contributorsText: "贡献者",
+        // custom containers
+        tip: "提示",
+        warning: "注意",
+        danger: "警告",
+        // 404 page
+        notFound: [
+          "这里什么都没有",
+          "我们怎么到这来了？",
+          "这是一个 404 页面",
+          "看起来我们进入了错误的链接",
+        ],
+        backToHome: "返回首页",
+        // a11y
+        openInNewWindow: "在新窗口打开",
+        toggleColorMode: "切换颜色模式",
+        toggleSidebar: "切换侧边栏",
+      },
+    },
+
+    themePlugins: {
+      // only enable git plugin in production mode
+      git: isProd,
+      // use shiki plugin in production mode instead
+      prismjs: !isProd,
+    },
+  }),
 });
---
+++ docs/.vuepress/configs/sidebar/index.js
@@ -0,0 +1,113 @@
+﻿export const sidebarZh = {
+  "/guide/": [
+    {
+      text: "指南",
+      children: ["/guide/README.md", "/guide/getting-started.md"],
+    },
+  ],
+  "/advanced/": [
+    {
+      text: "深入",
+      children: [
+        "/advanced/architecture.md",
+        "/advanced/plugin.md",
+        "/advanced/theme.md",
+      ],
+    },
+    {
+      text: "Cookbook",
+      children: [
+        "/advanced/cookbook/README.md",
+        "/advanced/cookbook/usage-of-client-config.md",
+        "/advanced/cookbook/adding-extra-pages.md",
+        "/advanced/cookbook/making-a-theme-extendable.md",
+        "/advanced/cookbook/passing-data-to-client-code.md",
+        "/advanced/cookbook/markdown-and-vue-sfc.md",
+      ],
+    },
+  ],
+  "/reference/": [
+    /*
+    {
+      text: "VuePress 参考",
+      collapsible: true,
+      children: [
+        "/reference/cli.md",
+        "/reference/config.md",
+        "/reference/frontmatter.md",
+        "/reference/components.md",
+        "/reference/plugin-api.md",
+        "/reference/theme-api.md",
+        "/reference/client-api.md",
+        "/reference/node-api.md",
+      ],
+    },
+    {
+      text: "打包工具参考",
+      collapsible: true,
+      children: ["/reference/bundler/vite.md", "/reference/bundler/webpack.md"],
+    },
+    {
+      text: "默认主题参考",
+      collapsible: true,
+      children: [
+        "/reference/default-theme/config.md",
+        "/reference/default-theme/frontmatter.md",
+        "/reference/default-theme/components.md",
+        "/reference/default-theme/markdown.md",
+        "/reference/default-theme/styles.md",
+        "/reference/default-theme/extending.md",
+      ],
+    },
+    {
+      text: "官方插件参考",
+      collapsible: true,
+      children: [
+        {
+          text: "常用功能",
+          children: [
+            "/reference/plugin/back-to-top.md",
+            "/reference/plugin/container.md",
+            "/reference/plugin/external-link-icon.md",
+            "/reference/plugin/google-analytics.md",
+            "/reference/plugin/medium-zoom.md",
+            "/reference/plugin/nprogress.md",
+            "/reference/plugin/register-components.md",
+          ],
+        },
+        {
+          text: "内容搜索",
+          children: [
+            "/reference/plugin/docsearch.md",
+            "/reference/plugin/search.md",
+          ],
+        },
+        {
+          text: "PWA",
+          children: [
+            "/reference/plugin/pwa.md",
+            "/reference/plugin/pwa-popup.md",
+          ],
+        },
+        {
+          text: "语法高亮",
+          children: [
+            "/reference/plugin/prismjs.md",
+            "/reference/plugin/shiki.md",
+          ],
+        },
+        {
+          text: "主题开发",
+          children: [
+            "/reference/plugin/active-header-links.md",
+            "/reference/plugin/git.md",
+            "/reference/plugin/palette.md",
+            "/reference/plugin/theme-data.md",
+            "/reference/plugin/toc.md",
+          ],
+        },
+      ],
+    },
+    */
+  ],
+};
