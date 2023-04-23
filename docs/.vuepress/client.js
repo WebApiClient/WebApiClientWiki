64386832967463cf56699f492fdadbfb580072ee
Ezreal <memory_of_you@hotmail.com>
2023-04-24 00:45:46 +0800

    Update client.js

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -14,19 +14,14 @@ export default defineClientConfig({
       ad.classList.add("wwads-cn", "wwads-vertical", "wwads-sticky");
       ad.setAttribute("data-id", "233");
       ad.style.maxWidth = "180px";
-      ad.style.position = "fixed";
-      ad.style.bottom = "0";
-      ad.style.right = "0";
-      ad.style.zIndex = "999";
-      document.body.appendChild(ad);
-
-      // const adDiv = document.createElement("div");
-      // adDiv.classList.add("wwads-cn", "wwads-horizontal");
-      // adDiv.setAttribute("data-id", "233");
-      // adDiv.style.maxWidth = "350px";
-
-      // // Add the created div element to the HTML DOM
-      // document.body.appendChild(adDiv);
+      // ad.style.position = "fixed";
+      // ad.style.bottom = "0";
+      // ad.style.right = "0";
+      // ad.style.zIndex = "999";
+      // document.body.appendChild(ad);
+      const body = document.querySelector("body");
+      const firstChild = body.firstElementChild;
+      body.insertBefore(ad, firstChild);
     }
   },
 });
