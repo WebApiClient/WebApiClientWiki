063bcac39476fc659ea2dd9edf2529ff0c5ca0aa
Ezreal <memory_of_you@hotmail.com>
2023-04-23 23:52:36 +0800

    Update client.js

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -8,10 +8,13 @@ export default defineClientConfig({
   },
   // rootComponents: [Ad],
   enhance({ app, router, siteData }) {
-    const ad = document.createElement("div");
-    ad.classList.add("wwads-cn", "wwads-vertical", "wwads-sticky");
-    ad.setAttribute("data-id", "233");
-    ad.style.maxWidth = "180px";
-    document.body.appendChild(ad);
+    if (typeof window !== "undefined") {
+      // 在浏览器环境中执行的代码...
+      const ad = document.createElement("div");
+      ad.classList.add("wwads-cn", "wwads-vertical", "wwads-sticky");
+      ad.setAttribute("data-id", "233");
+      ad.style.maxWidth = "180px";
+      document.body.appendChild(ad);
+    }
   },
 });
