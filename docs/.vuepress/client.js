4ebb1013ac92d991552e679947fbd71fa6cdd471
Ezreal <memory_of_you@hotmail.com>
2023-04-25 09:46:02 +0800

    innerWidth

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -14,18 +14,19 @@ export default defineClientConfig({
       ad.classList.add("wwads-cn", "wwads-vertical");
       ad.setAttribute("data-id", "233");
       // 判断屏幕PPI
-      if (window.devicePixelRatio <= 2) {
+      if (window.devicePixelRatio <= 2 && window.innerWidth > 480) {
         ad.style.maxWidth = "160px";
         ad.classList.add("wwads-sticky");
+        // ad.style.position = "fixed";
+        // ad.style.bottom = "0";
+        // ad.style.right = "0";
+        // ad.style.zIndex = "999";
+        const body = document.querySelector("body");
+        const firstChild = body.firstElementChild;
+        body.insertBefore(ad, firstChild);
+      } else {
+        document.body.appendChild(ad);
       }
-      // ad.style.position = "fixed";
-      // ad.style.bottom = "0";
-      // ad.style.right = "0";
-      // ad.style.zIndex = "999";
-      // document.body.appendChild(ad);
-      const body = document.querySelector("body");
-      const firstChild = body.firstElementChild;
-      body.insertBefore(ad, firstChild);
     }
   },
 });
